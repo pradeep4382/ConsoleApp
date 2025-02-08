1cfdfad287a959db58ef513616a453622de48936
pradeep4382 <41694249+pradeep4382@users.noreply.github.com>
2025-02-08 12:57:00 +0300

    Update Program.cs11

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-/////pradeep 98776544
+/////pradeep 888888
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
